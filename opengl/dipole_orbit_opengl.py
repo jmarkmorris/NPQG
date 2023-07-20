4436cff1a5efbbfe5b92be97f338f3268642e818
J Mark Morris <jmarkmorris@icloud.com>
2023-07-19 23:28:50 -0400

    Both manim and opengl versions are working.

--- opengl/dipole_orbit_opengl.py
+++ opengl/dipole_orbit_opengl.py
@@ -1,9 +1,3 @@
-# add every setting to the dictionary that would be specific to a point charge
-# scour both for more opportunities
-# this is a great learning technique
-# test
-# finally compare code and look for more optimizations
-
 # python3 dipole_orbit_opengl.py
 import pygame
 from pygame.locals import *
@@ -14,24 +8,25 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 import math
-from math import sin, cos, pi
+from math import sin, cos, tau
 import imageio
 
 # animation
 duration = 12
 frame_rate = 30
-frames_between_tracer_origins = [2, 3]
 paused = False
 
-potential_sphere_radius_increment = [0.015, 0.014, 0.013, 0.012, 0.011, 0.01, 0.008, 0.006]
+frames_between_tracer_origins = [6]
+potential_sphere_radius_increment = [0.03, 0.02]
+per_frame_increment_angle_in_radians = [2*tau/360]
 
-# angles in radians
-per_frame_increment_angle_in_radians = [2*TAU/360, 3*TAU/360]
-
-frame_width = 2999  # powerpoint png export size
-frame_height = 1687
+# powerpoint png export size
+# frame_width = 2999  
+# frame_height = 1687
 # frame_width = 2048
 # frame_height = 1280
+frame_width = 2000
+frame_height = 1200
 
 # tunables
 # the tracer radius increment vs. angle_in_radians increment is dualistic to v/c - giving the appearance of supra, par, or infra.
@@ -45,31 +40,30 @@ def draw_point_charge_representation(Q):
     x,y = Q["x"],Q["y"]
     glVertex2f(x,y)
     for i in range(361):
-        angle_in_radians = i * pi / 180
-        glVertex2f(x + Q[Q_key]["point_charge_representation_radius"] * cos(angle_in_radians), y + Q[Q_key]["point_charge_representation_radius"] * sin(angle_in_radians))
+        angle_in_radians = i * tau / 360
+        glVertex2f(x + Q["point_charge_representation_radius"] * cos(angle_in_radians), y + Q["point_charge_representation_radius"] * sin(angle_in_radians))
     glEnd()
 
 def draw_tracer_origins(Q):
     tracer_fade = 0
     tracer_fade_max = 1
-    tracer_fade_delta = 2 * tracer_fade_max / Q["num_tracer_origins"] / 3
+    tracer_fade_delta = (tracer_fade_max / Q["num_tracer_origins"])
     for cx, cy, cr in reversed(Q["tracer_origins"]):
         glBegin(GL_TRIANGLE_FAN)
         glColor3f(*[c if c == 1.0 else tracer_fade for c in Q["color"]])
         glVertex2f(cx, cy)
         for i in range(361):
-            glVertex2f(cx + radius=Q[Q_key]["tracer_origin_radius"] * cos(i * pi / 180), cy + radius=Q[Q_key]["tracer_origin_radius"] * sin(i * pi / 180))
+            glVertex2f(cx + Q["tracer_origin_radius"] * cos(i * tau / 360), cy + Q["tracer_origin_radius"] * sin(i * tau / 360))
         glEnd()
         tracer_fade = min(tracer_fade + tracer_fade_delta, tracer_fade_max)
 
 #——————————————————————————————————————————————————————————————————————————————
 
-# In opengl some helper functions also update the frame
 def calculate_sphere_fade_values(Q):
     sphere_fade_values = []
     sphere_fade = 1
     sphere_fade_min = 0
-    for _ in Q["potential_spheres"]:
+    for cx, cy, cr in Q["potential_spheres"]:
         sphere_fade_values.insert(0, sphere_fade)
         sphere_fade = max(sphere_fade - Q["sphere_fade_delta"], sphere_fade_min)
     return sphere_fade_values
@@ -82,22 +76,43 @@ def draw_potential_sphere(sphere, fade_value, color):
     # NOTE : If want to use anything other than G (0,1,0) like purple, we need to redesign this.
     color = (color[0] * fade_value, color[1] * fade_value, color[2] * fade_value)
     glColor3f(*color)
-    for i in range(360):
-        angle_in_radians = i * pi / 180
+    for i in range(361):
+        angle_in_radians = i * tau / 360
         glVertex2f(cx + cr * cos(angle_in_radians), cy + cr * sin(angle_in_radians))
     glEnd()
 
-def draw_recent_path(Q):
-    glBegin(GL_LINE_STRIP)
-    glColor3f(*color)
+# def draw_recent_path(Q):
+#     glBegin(GL_LINE_STRIP)
+#     glColor3f(*Q["color"])
 
-    loop_angle_in_radians = Q["angle_in_radians"] - Q["trailing_path_angle_in_radians)"]
+#     loop_angle_in_radians = Q["angle_in_radians"] - Q["trailing_path_angle_in_radians"]
+#     while loop_angle_in_radians < Q["angle_in_radians"] :
+#         path_x = cos(loop_angle_in_radians) * Q["dipole_orbit_radius"]
+#         path_y = sin(loop_angle_in_radians) * Q["dipole_orbit_radius"]
+#         glVertex2f(path_x, path_y)
+#         loop_angle_in_radians += Q["per_frame_increment_angle_in_radians"]
+#     glEnd()
+
+def draw_recent_path(Q):
+    # Enable blending
+    glEnable(GL_BLEND)
+    # Set blending function
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glBegin(GL_LINE_STRIP)
+
+    loop_angle_in_radians = Q["angle_in_radians"] - Q["trailing_path_angle_in_radians"]
     while loop_angle_in_radians < Q["angle_in_radians"] :
+        # Calculate alpha value based on current angle
+        alpha = (loop_angle_in_radians - (Q["angle_in_radians"] - Q["trailing_path_angle_in_radians"])) / Q["trailing_path_angle_in_radians"]
+        # Set color with alpha value
+        glColor4f(*Q["color"], alpha)
+
         path_x = cos(loop_angle_in_radians) * Q["dipole_orbit_radius"]
         path_y = sin(loop_angle_in_radians) * Q["dipole_orbit_radius"]
         glVertex2f(path_x, path_y)
-        loop_angle_in_radians += 1*TAU/360
+        loop_angle_in_radians += Q["per_frame_increment_angle_in_radians"]
     glEnd()
+
 
 #——————————————————————————————————————————————————————————————————————————————
 
@@ -107,34 +122,26 @@ def update_path(Q):
     Q["x"] = cos(Q["angle_in_radians"]) * Q["dipole_orbit_radius"]
     Q["y"] = sin(Q["angle_in_radians"]) * Q["dipole_orbit_radius"]
 
-    if frame_count % (frames_between_tracer_origins) == 0:
+    if frame_count % (Q["frames_between_tracer_origins"]) == 0:
         Q["potential_spheres"].append((Q["x"], Q["y"], 0))
 
-    Q["sphere_fade_values"] = calculate_sphere_fade_values(Q["potential_spheres"])
+    Q["sphere_fade_values"] = calculate_sphere_fade_values(Q)
 
-    # draw spheres with calculated sphere_fade values but do so in alternating fashion so that the order of spheres drawn looks good.
+    draw_recent_path(Q)
+    draw_point_charge_representation(Q)
+    draw_tracer_origins(Q)
     for index, potential_sphere in enumerate(Q["potential_spheres"]) :
-        fade_value = Q["sphere_fade_values"][index]
-        sphere = draw_potential_sphere(potential_sphere, fade_value, Q["color"]))
-        updated_path.add(sphere)
+        draw_potential_sphere(potential_sphere, Q["sphere_fade_values"][index], Q["color"])
             
-        draw_recent_path(Q)
 
-        draw_point_charge_representation(Q)
-
-    if frame_count % (frames_between_tracer_origins) == 0:
-        # print(f"frame_count: {frame_count}")
-
+    if frame_count % (Q["frames_between_tracer_origins"]) == 0:
         Q["tracer_origins"].append((Q["x"], Q["y"], 0))
         if len(Q["tracer_origins"]) > Q["num_tracer_origins"]:
             Q["tracer_origins"].pop(0)
 
-        Q["potential_spheres"] = [(cx, cy, cr + potential_sphere_radius_increment/frames_between_tracer_origins) for cx, cy, cr in Q["potential_spheres"] if cr < 2]
+    Q["potential_spheres"] = [(cx, cy, cr + Q["potential_sphere_radius_increment"]/Q["frames_between_tracer_origins"]) for cx, cy, cr in Q["potential_spheres"] if cr < 2]
+    Q["angle_in_radians"] += Q["per_frame_increment_angle_in_radians"]
 
-        draw_tracer_origins(Q)
-
-    Q["angle_in_radians"] += per_frame_increment_angle_in_radians
-    frame_count +=1
 
 pygame.init()
 pygame.font.init()
@@ -152,55 +159,48 @@ for a_radius in potential_sphere_radius_increment:
             frames = []
             frame_count = 0
 
-            # Add the initial circles and tracer_origins to the dictionary.
-            self.Q = {
+            Q = {
                 "blue": {
-                    "circle": [],
-                    "angle_in_radians": 0,
-                    "trailing_path_angle_in_radians": 130*TAU/360,
-                    "potential_sphere_radius_increment": a_fbt,
-                    "per_frame_increment_angle_in_radians": b_fbt,
+                    "angle_in_radians": tau/2,
+                    "trailing_path_angle_in_radians": 130*tau/360,
+                    "potential_sphere_radius_increment": a_radius,
+                    "per_frame_increment_angle_in_radians": b_angle_in_radians_increment,
                     "frames_between_tracer_origins": c_fbt,
-                    "num_tracer_origins": math.floor(self.Q["trailing_path_angle_in_radians"] / (self.Q["frames_between_tracer_origins"]*self.Q["per_frame_increment_angle_in_radians"])),
+                    "num_tracer_origins": 1,
                     "opacity": 1,
                     "color": (0.0, 0.0, 1.0),
                     "tracer_origins": [],
-                    "x": -1,
+                    "x": -0.4,
                     "y": 0,
-                    "dipole_orbit_radius": 2.0,
-                    "point_charge_representation_radius": 0.1,
-                    "tracer_origin_radius": 0.02,
-                    "sphere_fade_delta": 0.010,
-                    "initial_position": LEFT,
+                    "dipole_orbit_radius": 0.5,
+                    "point_charge_representation_radius": 0.03,
+                    "tracer_origin_radius": 0.01,
+                    "sphere_fade_delta": 0.020,
                     "potential_spheres": []
                 },
                 "red": {
-                    "circle": [],
-                    "angle_in_radians": PI,
-                    "trailing_path_angle_in_radians": 130*TAU/360,
-                    "potential_sphere_radius_increment": a_fbt,
-                    "per_frame_increment_angle_in_radians": b_fbt,
+                    "angle_in_radians": 0,
+                    "trailing_path_angle_in_radians": 130*tau/360,
+                    "potential_sphere_radius_increment": a_radius,
+                    "per_frame_increment_angle_in_radians": b_angle_in_radians_increment,
                     "frames_between_tracer_origins": c_fbt,
-                    "num_tracer_origins": math.floor(self.Q["trailing_path_angle_in_radians"] / (self.Q["frames_between_tracer_origins"]*self.Q["per_frame_increment_angle_in_radians"])),
+                    "num_tracer_origins": 1,
                     "opacity": 1,
                     "color": (1.0, 0.0, 0.0),
                     "tracer_origins": [],
-                    "x": 1,
+                    "x": 0.4,
                     "y": 0,
-                    "dipole_orbit_radius": 2.0,
-                    "point_charge_representation_radius": 0.1,
-                    "tracer_origin_radius": 0.02,
-                    "sphere_fade_delta": 0.010,
-                    "initial_position": RIGHT,
+                    "dipole_orbit_radius": 0.5,
+                    "point_charge_representation_radius": 0.03,
+                    "tracer_origin_radius": 0.01,
+                    "sphere_fade_delta": 0.020,
                     "potential_spheres": []
                 }
             }
-            for Q_key in self.Q:
-                self.Q[Q_key]["circle"] = Circle(color=self.Q[Q_key]["color"], radius=self.Q[Q_key]["point_charge_representation_radius"], fill_opacity=1).shift(self.Q[Q_key]["initial_position"] * self.Q[Q_key]["dipole_orbit_radius"])
-                self.Q[Q_key]["sphere_fade_values"] = calculate_sphere_fade_values(self.Q[Q_key]["potential_spheres"])
-                self.Q[Q_key]["num_tracer_origins"] = self.Q[Q_key]["trailing_path_angle_in_radians"] / (self.Q[Q_key]["frames_between_tracer_origins"]*self.Q[Q_key]["per_frame_increment_angle_in_radians"]) - 2
-                self.Q[Q_key]["x"] = self.Q[Q_key]["x"] * self.Q[Q_key]["dipole_orbit_radius"]
-
+            for Q_key in Q:
+                Q[Q_key]["sphere_fade_values"] = calculate_sphere_fade_values(Q[Q_key])
+                Q[Q_key]["num_tracer_origins"] = Q[Q_key]["trailing_path_angle_in_radians"] / (Q[Q_key]["frames_between_tracer_origins"]*Q[Q_key]["per_frame_increment_angle_in_radians"]) - 1
+                draw_recent_path(Q[Q_key])
 
             while frame_count < duration * frame_rate:
 
@@ -217,13 +217,13 @@ for a_radius in potential_sphere_radius_increment:
 
                 if not paused:
                     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-                    update_path(self.Q, a_radius, b_angle_in_radians_increment, c_fbt)
-                    filename = f"dipole-{a_radius}-{b_angle_in_radians_increment}-{c_fbt}.mp4"
+                    for Q_key in Q:
+                        update_path(Q[Q_key])
 
                     label_text = f"potential_sphere_radius_increment: {a_radius}, per_frame_increment_angle_in_radians: {b_angle_in_radians_increment}, frames_between_tracer_origins: {c_fbt}"
-                    x, y = -0.65, 0.9
+                    x, y = -1.2, 0.9
                     glColor3f(0.5, 0.0, 0.5) # set the color to purple
-                    glRectf(x-.1, y-.03, x + 1.3, y+.05) # draw the rectangle
+                    glRectf(x-0.1, y-.03, x + 2.6, y+.05) # draw the rectangle
                     glColor3f(1, 1, 1) # white color
                     glRasterPos2f(x, y)
                     for character in label_text:
@@ -239,7 +239,9 @@ for a_radius in potential_sphere_radius_increment:
                     # This will make the mp4 run in the reverse direction
                     # image = pygame.transform.flip(image, False, True)
                     frames.append(pygame.surfarray.array3d(image))
+                    frame_count +=1
 
+            filename = f"dipole-{a_radius}-{b_angle_in_radians_increment}-{c_fbt}.mp4"
             imageio.mimwrite(filename, frames, fps=frame_rate)
 
 pygame.quit()
