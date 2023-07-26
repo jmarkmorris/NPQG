4d6d70c5b393be1015396bed0ef0553fa484f02e
J Mark Morris <jmarkmorris@icloud.com>
2023-07-26 07:49:12 -0400

    ported circle sizes from opengl to manim

--- manim/circlesizes.py
+++ manim/circlesizes.py
@@ -1,41 +1,5 @@
 # manim -pqh --disable_caching circlesizes.py circlesizes -p
 
-# from manim import *
-
-# # import math
-# # import sys
-# # from ctypes import *
-
-# # frame_rate = 60
-# config.pixel_width = 2998
-# config.pixel_height = 1686
-# # config.frame_rate = frame_rate
-
-# INDIGO = "#4B0082"
-
-# colors = [PURE_BLUE, PURE_RED]
-
-# class circlesizes(Scene):
-#     def construct(self):
-#         self.camera.background_color = INDIGO # looks good
-
-
-#         grid = NumberPlane(
-#             x_range=[-7, 7],
-#             y_range=[-4, 4],
-#             axis_config={
-#                 "stroke_color": WHITE,
-#                 "stroke_width": 1,
-#                 "include_ticks": False,
-#                 "include_tip": False,
-#             },
-#             background_line_style={
-#                 "stroke_color": ELECTRIC_PURPLE,
-#                 "stroke_width": 1,
-#             }
-#         )
-#         self.add(grid)
-
 
 from manim import *
 
@@ -64,14 +28,19 @@ class circlesizes(Scene):
         )
         self.add(grid)
 
-        for x in range(-7, 7):
-            for y in range(-4, 4):
-                left_circle = Circle(radius=0.2, color=colors[0]).move_to([x + 0.25, y+0.75, 0])
+        radius = 0.002
+        for y in range(4, -4, -1):
+            for x in range(-7, 7):
+                left_circle = Circle(radius=radius, color=colors[0]).move_to([x + 0.25, y-0.25, 0])
                 left_circle.set_fill(color=colors[0], opacity=1)
-                right_circle = Circle(radius=0.2, color=colors[1]).move_to([x + 0.75, y+0.75, 0])
+                right_circle = Circle(radius=radius, color=colors[1]).move_to([x + 0.75, y-0.25, 0])
                 right_circle.set_fill(color=colors[1], opacity=1)
                 self.add(left_circle, right_circle)
 
+                label = Text(f"r = {radius:.3f}", color=WHITE, font_size=16, weight=BOLD).move_to([x + 0.5, y-0.75, 0])
+                self.add(label)
+
+                radius += 0.002
 
         self.wait(0.5)
 
