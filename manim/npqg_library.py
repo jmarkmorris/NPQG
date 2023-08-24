30f298c02a48ee275d438368a4a71bb69fcd5546
J Mark Morris <jmarkmorris@icloud.com>
2023-08-23 23:32:15 -0400

    some cool ideas about a library

--- manim/npqg_library.py
+++ manim/npqg_library.py
@@ -1,7 +1,7 @@
 # This is a library of reusable NPQG software.
 # 
 # The intent is to have an object oriented hierarchy.
-# First pass ideas...
+# First pass ideas...not sure if these are at the right granularity. attempting to build up from the fundamentals.
 # point_charge : Fundamental
 # binary : inherits  point_charge
 # dual_binary : inherits binary
@@ -11,7 +11,9 @@
 # personality_duo : inherits personality_charge (using the right hand rule we can be specific on the geometry)
 # personality : inherits personality_duo, color - where color corresponds to generation or number of binaries in the nest.
 # I wonder if anyone has ever thought of mapping color charge to generations of quarks?  Hmmm, that's an interesting new mapping.
-# fermion : inherits 
+# fermion : inherits Noether_core, personality
+
+# path : A fundamental concept that is emergent according to the evolution equation in absolute time and absolute space.
 
 
 
